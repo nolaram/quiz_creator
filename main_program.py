6d52813f2a70fa108ce4b2183c160e07495f6537
nolaram <marloncopino10@gmail.com>
2025-04-09 04:19:59 +0800

    create_file_for_built_quiz

--- main_program.py
+++ main_program.py
@@ -1,5 +1,9 @@
 # import all libraries needed for the program
 # main loop for asking questions
+
+def main():
+    file_name = 'quiz_questions.txt'
+    
     # options
     # correct answer
 # exit program
