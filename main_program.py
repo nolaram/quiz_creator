6e4a5109f370073481345757ac26767f7ae1ceff
nolaram <marloncopino10@gmail.com>
2025-04-09 04:27:09 +0800

    loop_for_input_question_and_answer

--- main_program.py
+++ main_program.py
@@ -5,6 +5,13 @@ def main():
     file_name = 'quiz_questions.txt'
 
     with open(file_name, 'a') as file:
-    # options
+        while True:
+            print('-- Add a new Question --')
+            question = input("Enter a question: ")
+            option_a = input("Enter option A: ")
+            option_b = input("Enter option B: ")
+            option_c = input("Enter option C: ")
+            option_d = input("Enter option D: ")
+            
     # correct answer
 # exit program
