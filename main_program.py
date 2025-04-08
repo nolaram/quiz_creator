01916134696f605e8f745017bd32c32a1f939a2a
nolaram <marloncopino10@gmail.com>
2025-04-09 04:58:53 +0800

    added_comments

--- main_program.py
+++ main_program.py
@@ -1,6 +1,5 @@
 # import all libraries needed for the program
 # main loop for asking questions
-
 def main():
     file_name = 'quiz_questions.txt'
 
@@ -33,8 +32,9 @@ def main():
             repeat = input("Do you want to add another question? (yes/no): ").strip().lower()
             if repeat != 'yes':
                 print("Thank you!")
+                # exit program
                 break
 
-# exit program
+# go back to main
 if __name__ == '__main__':
     main()
