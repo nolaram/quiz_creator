026c2e6aa151f51e2d5cce5b886d54742087740a
nolaram <marloncopino10@gmail.com>
2025-05-01 21:10:53 +0800

    change_variable_name_add_list_and_dictionary

--- quiz_reader/quiz_reader.py
+++ quiz_reader/quiz_reader.py
@@ -1,8 +1,11 @@
 # read the file
-def read_and_display_quiz_file(quiz_file_path):
+def load_questions_from_file(quiz_file_path):
+    question_list = []
+
     try:
         # open the file
         with open(quiz_file_path, 'r', encoding='utf-8') as quiz_file:
+            current_question_data = {}
             # read the entire content of the file
             file_contents = quiz_file.read()
         # display the file content of the file
@@ -18,4 +21,4 @@ def read_and_display_quiz_file(quiz_file_path):
 
 if __name__ == "__main__":
     quiz_file_name = 'quiz_questions.txt'
-    read_and_display_quiz_file(quiz_file_name)
+    loaded_questions = load_questions_from_file(quiz_file_name)
