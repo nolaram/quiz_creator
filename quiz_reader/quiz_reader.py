58fdf83a88adb34ef9eee25a8cf3468ad17ac47a
nolaram <marloncopino10@gmail.com>
2025-05-01 21:00:18 +0800

    read_file

--- quiz_reader/quiz_reader.py
+++ quiz_reader/quiz_reader.py
@@ -1,7 +1,12 @@
 # read the file
-    # open the file
-    # read the entire content of the file
-    # display the file content of the file
+def read_and_display_quiz_file(quiz_file_path):
+    try:
+        # open the file
+        with open(quiz_file_path, 'r', encoding='utf-8') as quiz_file:
+            # read the entire content of the file
+            file_contents = quiz_file.read()
+        # display the file content of the file
+        print(file_contents)
 # if file is not found
     # print the file is not found
 # if there are other errors
