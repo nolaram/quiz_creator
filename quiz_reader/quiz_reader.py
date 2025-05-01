61a1f4b5301d4f8bf4e83ed7f1854ba339c230e3
nolaram <marloncopino10@gmail.com>
2025-05-01 20:57:27 +0800

    pseudocode_for_quiz_reader

---
+++ quiz_reader/quiz_reader.py
@@ -0,0 +1,8 @@
+# read the file
+    # open the file
+    # read the entire content of the file
+    # display the file content of the file
+# if file is not found
+    # print the file is not found
+# if there are other errors
+    # print the error
